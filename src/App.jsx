4d1f9b41735deedf26bb98e504c5f4a4f078ccef
ASANIYAN AYOMIDE PAUL <ayomidepaulasaniyan@gmail.com>
2023-05-24 15:30:15 +0100

    created authenticated page component alongside sign-in form

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import "./categories.styles.scss";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const App = () => {
 
@@ -13,7 +13,7 @@ const App = () => {
         <Route path="/"  element={<Navigation />} >
           <Route index  element={<Home />} />
           <Route path="shop" element={<div>I am the shop</div>} />  
-          <Route path="sign-in" element={<SignIn />} />  
+          <Route path="auth" element={<Authentication />} />  
         </Route>
         
       </>
